acddae797e5ce9c08b2388c15afc91b11d999bb6
Alexander Litvinov <axndr.litvinov@gmail.com>
2023-10-25 19:08:24 +0200

    cache is working?

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,32 +1,43 @@
-from collections import deque
 from collections.abc import Hashable
 from typing import Any
 
 
 class LRUCache:
+    __slots__ = ["data", "deck", "limit"]
+
     def __init__(self, limit: int = 42) -> None:
         self.data: dict[Hashable, Any] = {}
-        self.deck: deque[Hashable] = deque(maxlen=limit + 1)
+        self.deck: dict[Hashable, None] = {}
         self.limit = limit
 
     def get(self, key: Hashable) -> Any | None:
         try:
-            value = self.data[key]
+            value = self.data[key]  # O(1)
         except KeyError:
             return None
         else:
-            if key != self.deck[-1]:
-                self.deck.append(key)
+            if key not in self.deck:  # O(1)
+                self.deck[key] = None  # O(1)
+            else:
+                del self.deck[key]  # O(1)
+                self.deck[key] = None  # O(1)
 
-            if len(self.deck) > len(self.data):
-                self.deck.popleft()
+            if len(self.deck) > len(self.data):  # O(1)
+                first_key = next(iter(self.deck))  # O(1)
+                del self.deck[first_key]  # O(1)
             return value
 
-    def set(self, key, value) -> None:
-        self.data[key] = value
-        self.deck.append(key)
-        if len(self.data) == self.limit + 1:
-            self.data.pop(self.deck.popleft())
+    def set(self, key: Hashable, value: Any) -> None:
+        self.data[key] = value  # O(1)
+        if key not in self.deck:  # O(1)
+            self.deck[key] = None  # O(1)
+        else:
+            del self.deck[key]  # O(1)
+            self.deck[key] = None  # O(1)
+
+        if len(self.data) == self.limit + 1:  # O(1)
+            first_key = next(iter(self.deck))  # O(1)
+            del self.data[first_key]  # O(1)
 
     def __repr__(self) -> str:
-        return repr(self.data)
+        return repr({k: self.data[k] for k in self.deck if k in self.data})
