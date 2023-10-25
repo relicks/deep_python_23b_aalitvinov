31a0fcabc7ea9fbf0ddcc4dc8888e5e8ef3e7bb4
Alexander Litvinov <axndr.litvinov@gmail.com>
2023-10-25 18:40:27 +0200

    progressing

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,9 +1,32 @@
+from collections import deque
+from collections.abc import Hashable
+from typing import Any
+
+
 class LRUCache:
-    def __init__(self, limit=42):
-        raise NotImplementedError
+    def __init__(self, limit: int = 42) -> None:
+        self.data: dict[Hashable, Any] = {}
+        self.deck: deque[Hashable] = deque(maxlen=limit + 1)
+        self.limit = limit
 
-    def get(self, key):
-        raise NotImplementedError
+    def get(self, key: Hashable) -> Any | None:
+        try:
+            value = self.data[key]
+        except KeyError:
+            return None
+        else:
+            if key != self.deck[-1]:
+                self.deck.append(key)
 
-    def set(self, key, value):
-        raise NotImplementedError
+            if len(self.deck) > len(self.data):
+                self.deck.popleft()
+            return value
+
+    def set(self, key, value) -> None:
+        self.data[key] = value
+        self.deck.append(key)
+        if len(self.data) == self.limit + 1:
+            self.data.pop(self.deck.popleft())
+
+    def __repr__(self) -> str:
+        return repr(self.data)
--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -1,2 +1,26 @@
-def test_t2():
-    ...
+from lru_cache import LRUCache
+
+
+def test_given():
+    cache = LRUCache(2)
+
+    cache.set("k1", "val1")
+    cache.set("k2", "val2")
+
+    assert cache.get("k3") is None
+    assert cache.get("k2") == "val2"
+    assert cache.get("k1") == "val1"
+
+    cache.set("k3", "val3")
+
+    assert cache.get("k3") == "val3"
+    assert cache.get("k2") is None
+    assert cache.get("k1") == "val1"
+
+
+def test_bad_case():
+    cache = LRUCache(4)
+
+    cache.set("k1", "val1")
+    cache.set("k2", "val2")
+    cache.set("k3", "val3")
