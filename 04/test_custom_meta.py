a1ca58661c36d5287ff5f7265d954ad5ed852a7a
Alexander Litvinov <axndr.litvinov@gmail.com>
2023-10-25 13:45:01 +0200

    added tests

--- 04/test_custom_meta.py
+++ 04/test_custom_meta.py
@@ -2,7 +2,7 @@
 # pylint: disable=redefined-outer-name
 import pytest
 
-from custom_meta import CustomMeta
+from custom_meta import CustomMeta, is_dunder_name, prefix_dict
 
 CLASS_VAR = 50
 INST_VAR = "999"
@@ -73,3 +73,40 @@ def test_inst_attrs_set(custom_class: type):
     inst = custom_class()
     inst.dynamic = "added later"
     assert inst.custom_dynamic == "added later"
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        ("nme", False),
+        ("not_a_dunder_name", False),
+        ("____", False),
+        ("__name", False),
+        ("what__", False),
+        ("__ява__", False),
+        ("__ggg__", True),
+        ("__?__", True),
+    ],
+)
+def test_is_dunder_name(name: tuple[str, bool]):
+    assert is_dunder_name(name[0]) == name[1]
+
+
+def test_prefix_dict():
+    prefix = "c_pr_"
+    sample_dict = {
+        "regular_name": object,
+        "__stupid_name": ...,
+        "ridiculous__": 4 + 2j,
+        f"{prefix}art": 9,
+        "__special__": False,
+    }
+    expected_dict = {
+        f"{prefix}regular_name": object,
+        f"{prefix}art": 9,
+        f"{prefix}__stupid_name": ...,
+        f"{prefix}ridiculous__": 4 + 2j,
+        "__special__": False,
+    }
+
+    assert prefix_dict(sample_dict, prefix) == expected_dict
