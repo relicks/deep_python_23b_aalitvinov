072dc01735a040c9527760156483b1b2e08f5fcf
Alexander Litvinov <axndr.litvinov@gmail.com>
2023-10-26 12:31:06 +0200

    added limit validation

--- 05/lru_cache.py
+++ 05/lru_cache.py
@@ -1,47 +1,89 @@
 """Содержит решение первого пункта HW05."""
+from abc import ABC, abstractmethod
 from collections.abc import Hashable
 from typing import Any
 
 
+# from https://docs.python.org/3/howto/descriptor.html#validator-class
+class Validator(ABC):  # from hw04 # pragma: no cover
+    """Abstract base class for Validator data descriptors."""
+
+    def __set_name__(self, owner: type, name: str):
+        # pylint: disable-next=attribute-defined-outside-init
+        self._private_name = "_" + name
+
+    def __get__(self, obj: object, objtype: type | None = None):
+        return getattr(obj, self._private_name)
+
+    def __set__(self, obj: object, value: Any):
+        err = self.validate(value)
+        if err is not None:
+            raise err
+        setattr(obj, self._private_name, value)
+
+    @abstractmethod
+    def validate(self, value: Any) -> Exception | None:
+        pass
+
+
+class NaturalIntValidator(Validator):
+    """Data descriptor that checks input to be valid positive int."""
+
+    def validate(self, value: int) -> TypeError | ValueError | None:
+        if not isinstance(value, int):
+            return TypeError(f"Expected {value!r} to be of type int.")
+        if not value > 0:
+            return ValueError(f"Expected {value!r} to be positive.")
+        return None
+
+
 class LRUCache:
-    __slots__ = ["data", "deck", "limit"]
+    """LRU cache implementation based on two `dict`s.
+
+    Time complexity of `get` and `set` methods is O(1) on average and
+    O(n) in the worst case.
+
+    :param limit: The size of LRU cache, must be positive
+    """
+
+    _limit = NaturalIntValidator()
 
     def __init__(self, limit: int = 42) -> None:
-        self.data: dict[Hashable, Any] = {}
-        self.deck: dict[Hashable, None] = {}
-        self.limit = limit
+        self._data: dict[Hashable, Any] = {}
+        self._deck: dict[Hashable, None] = {}
+        self._limit = limit
 
     def get(self, key: Hashable) -> Any | None:
         try:
-            value = self.data[key]  # O(1)
+            value = self._data[key]  # O(1)
         except KeyError:
             return None
 
-        if key not in self.deck:  # O(1)
-            self.deck[key] = None  # O(1)
+        if key not in self._deck:  # O(1)
+            self._deck[key] = None  # O(1)
         else:
-            del self.deck[key]  # O(1)
-            self.deck[key] = None  # O(1)
+            del self._deck[key]  # O(1)
+            self._deck[key] = None  # O(1)
 
-        if len(self.deck) > len(self.data):  # O(1)
-            first_key = next(iter(self.deck))  # O(1)
-            del self.deck[first_key]  # O(1)
+        if len(self._deck) > len(self._data):  # O(1)
+            first_key = next(iter(self._deck))  # O(1)
+            del self._deck[first_key]  # O(1)
         return value
 
     def set(self, key: Hashable, value: Any) -> None:
-        self.data[key] = value  # O(1)
-        if key not in self.deck:  # O(1)
-            self.deck[key] = None  # O(1)
+        self._data[key] = value  # O(1)
+        if key not in self._deck:  # O(1)
+            self._deck[key] = None  # O(1)
         else:
-            del self.deck[key]  # O(1)
-            self.deck[key] = None  # O(1)
+            del self._deck[key]  # O(1)
+            self._deck[key] = None  # O(1)
 
-        if len(self.data) == self.limit + 1:  # O(1)
-            first_key = next(iter(self.deck))  # O(1)
-            del self.data[first_key]  # O(1)
+        if len(self._data) == self._limit + 1:  # O(1)
+            first_key = next(iter(self._deck))  # O(1)
+            del self._data[first_key]  # O(1)
 
     def to_dict(self) -> dict[Hashable, Any]:
-        return {k: self.data[k] for k in self.deck if k in self.data}
+        return {k: self._data[k] for k in self._deck if k in self._data}
 
     def __repr__(self) -> str:
         return repr(self.to_dict())
--- 05/test_lru_cache.py
+++ 05/test_lru_cache.py
@@ -1,36 +1,70 @@
 """Содержит второй пункт (тесты решения первого пункта) HW05."""
-from lru_cache import LRUCache
+import pytest
+
+from lru_cache import LRUCache, NaturalIntValidator
 
 
-def test_given():
-    cache = LRUCache(2)
+class TestNaturalIntValidator:
+    @pytest.fixture()
+    def example_class(self):
+        class Example:
+            field = NaturalIntValidator()
 
-    cache.set("k1", "val1")
-    cache.set("k2", "val2")
+            def __init__(self, field) -> None:
+                self.field = field
 
-    assert cache.get("k3") is None
-    assert cache.get("k2") == "val2"
-    assert cache.get("k1") == "val1"
+        return Example
 
-    cache.set("k3", "val3")
+    def test_validate(self, example_class: type):
+        inst = example_class(15)
+        assert inst.field == 15
+        inst.field = 1
+        assert inst.field == 1
 
-    assert cache.get("k3") == "val3"
-    assert cache.get("k2") is None
-    assert cache.get("k1") == "val1"
+    def test_validate_raises(self, example_class: type):
+        with pytest.raises(TypeError):
+            example_class("number")
+
+        with pytest.raises(ValueError, match="positive"):
+            example_class(0)
+
+        inst = example_class(2)
+        with pytest.raises(TypeError):
+            inst.field = "number"
+
+        with pytest.raises(ValueError, match="positive"):
+            inst.field = 0
 
 
-def test_bad_case():
-    cache = LRUCache(3)
+class TestLRUCache:
+    def test_given(self):
+        cache = LRUCache(2)
 
-    cache.set("k1", "val1")
-    cache.set("k2", "val2")
-    cache.set("k3", "val3")
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
 
-    assert cache.get("k2") == "val2"
-    assert cache.get("k1") == "val1"
-    assert cache.get("k2") == "val2"
+        assert cache.get("k3") is None
+        assert cache.get("k2") == "val2"
+        assert cache.get("k1") == "val1"
 
-    cache.set("k4", "val4")
+        cache.set("k3", "val3")
 
-    assert cache.get("k3") is None
-    assert all(cache.get(k) is not None for k in ("k1", "k2", "k4"))
+        assert cache.get("k3") == "val3"
+        assert cache.get("k2") is None
+        assert cache.get("k1") == "val1"
+
+    def test_bad_case(self):
+        cache = LRUCache(3)
+
+        cache.set("k1", "val1")
+        cache.set("k2", "val2")
+        cache.set("k3", "val3")
+
+        assert cache.get("k2") == "val2"
+        assert cache.get("k1") == "val1"
+        assert cache.get("k2") == "val2"
+
+        cache.set("k4", "val4")
+
+        assert cache.get("k3") is None
+        assert all(cache.get(k) is not None for k in ("k1", "k2", "k4"))
