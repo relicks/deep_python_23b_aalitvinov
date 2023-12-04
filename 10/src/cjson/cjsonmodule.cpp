355d96aa151d660e08bc9d630f28db9e543a8902
Alexander Litvinov <axndr.litvinov@gmail.com>
2023-12-04 21:12:49 +0100

    first iteration

--- 10/src/cjson/cjsonmodule.cpp
+++ 10/src/cjson/cjsonmodule.cpp
@@ -1,11 +1,13 @@
+#include <Python.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <Python.h>
-#include <iostream>
+#include <nlohmann/json.hpp>
 
-PyObject *cjson_sum(PyObject *self, PyObject *args) {
-    PyObject *list_obj;
+using json = nlohmann::json;
+
+PyObject* cjson_sum(PyObject* self, PyObject* args) {
+    PyObject* list_obj;
     // long int elem_count;
     if (!PyArg_ParseTuple(args, "O", &list_obj)) {
         printf("Failed to parse arguments");
@@ -15,37 +17,69 @@ PyObject *cjson_sum(PyObject *self, PyObject *args) {
     long int list_len = PyList_Size(list_obj);
     int res = 0;
     for (int i = 0; i < list_len && i < list_len; ++i) {
-        PyObject *tmp = PyList_GetItem(list_obj, i);
+        PyObject* tmp = PyList_GetItem(list_obj, i);
         int elem = PyLong_AsLong(tmp);
         res += elem;
     }
     return Py_BuildValue("i", res);
 }
 
-int fibonacci(int n) {
-    if (n < 2) {
-        return 1;
-    }
-    return fibonacci(n - 1) + fibonacci(n - 2);
-}
-
-PyObject *cjson_fibonacci(PyObject *self, PyObject *args) {
-    int n = 0;
-    if (!PyArg_ParseTuple(args, "i", &n)) {
-        printf("Failed to parse arguments");
+PyObject* cjson_loads(PyObject* self, PyObject* args) {
+    char* json_str;
+    if (!PyArg_ParseTuple(args, "s", &json_str)) {
         return NULL;
     }
-    int res = fibonacci(n);
-    return Py_BuildValue("i", res);
+
+    json j = json::parse(json_str);
+
+    // Convert the JSON object to a Python dictionary
+    PyObject* dict = PyDict_New();
+    for (auto& element : j.items()) {
+        PyDict_SetItemString(
+            dict,
+            element.key().c_str(),
+            PyUnicode_FromString(element.value().dump().c_str())
+        );
+    }
+
+    return dict;
+}
+
+PyObject* cjson_dumps(PyObject* self, PyObject* args) {
+    PyObject* dict;
+    if (!PyArg_ParseTuple(args, "O", &dict)) {
+        return NULL;
+    }
+
+    // Convert the Python dictionary to a JSON object
+    json j;
+    PyObject *key, *value;
+    Py_ssize_t pos = 0;
+    while (PyDict_Next(dict, &pos, &key, &value)) {
+        j[PyUnicode_AsUTF8(key)] = PyUnicode_AsUTF8(value);
+    }
+
+    // Convert the JSON object to a string
+    std::string str = j.dump();
+    return PyUnicode_FromString(str.c_str());
 }
 
 static PyMethodDef methods[] = {
     {"sum", cjson_sum, METH_VARARGS, "sum of first n elements of our array"},
-    {"fibonacci", cjson_fibonacci, METH_VARARGS, "returns i'th element of fibonacci sequance"},
-    {NULL, NULL, 0, NULL}};
+    {"loads",
+     cjson_loads,
+     METH_VARARGS,
+     "Deserialize a str instance containing a JSON document to a Python object."
+    },
+    {"dumps",
+     cjson_dumps,
+     METH_VARARGS,
+     "Serialize a Python dict to a JSON formatted str."},
+    {NULL, NULL, 0, NULL}
+};
 
-static struct PyModuleDef module_cjson = {
-    PyModuleDef_HEAD_INIT, "cjson", NULL, -1, methods};
+static struct PyModuleDef module_cjson =
+    {PyModuleDef_HEAD_INIT, "cjson", NULL, -1, methods};
 
 PyMODINIT_FUNC PyInit_cjson() {
     return PyModule_Create(&module_cjson);
--- 10/tests/test_cjson.py
+++ 10/tests/test_cjson.py
@@ -1,9 +1,17 @@
-# import json
+import json
 
 import cjson
-
-# import ujson
+import ujson
 
 
 def test_one():
     assert cjson.sum([1, 3, 5]) == 9
+
+
+def test_parsers():
+    json_str = '{"hello": 10, "world": "value"}'
+    json_doc = json.loads(json_str)
+    ujson_doc = ujson.loads(json_str)
+    cjson_doc = cjson.loads(json_str)
+    assert json_doc == ujson_doc == cjson_doc
+    assert json_str == cjson.dumps(cjson.loads(json_str))
