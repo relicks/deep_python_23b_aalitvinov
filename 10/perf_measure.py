7ba3c8792441fb4bd380bd39d4c26a051016a196
Alexander Litvinov <axndr.litvinov@gmail.com>
2023-12-07 15:23:55 +0100

    added performance measurements

---
+++ 10/perf_measure.py
@@ -0,0 +1,69 @@
+import json
+import statistics as st
+import timeit
+
+import cjson
+import ujson
+
+
+def bench(func, n_runs=30, n_loops=1_000):
+    race = timeit.repeat(func, repeat=n_runs, number=n_loops)
+    race = [run / n_loops for run in race]
+    infimum = round(min(race) * 10**9)
+    mean = round(st.mean(race) * 10**9)
+    std = round(st.stdev(race) * 10**9, 1)
+    print(
+        f"\033[1m{mean} ns ± {std} ns\033[0m per loop"
+        + f" (mean ± std. dev. of {n_runs} runs, {n_loops} loops each)"
+        + f"\nbest time \033[1m\033[32m{infimum}\033[39m ns\033[0m"
+    )
+    return {"mean": mean, "stdev": std, "min": infimum}
+
+
+def testing_loads(json_str: str):
+    print("Testing 'loads' method...\n")
+    print("\nTesting stdlib json 'loads' performance:")
+    print("================================")
+    json_results = bench(lambda: json.loads(json_str))
+
+    print("\nTesting ujson 'loads' performance:")
+    print("==========================")
+    ujson_results = bench(lambda: ujson.loads(json_str))
+    print(f"{ujson_results['mean'] / json_results['mean']:.2f} of stdlib json time")
+
+    print("\nTesting my very own cjson parser 'loads' performance 😢:")
+    print("=========================================")
+    cjson_results = bench(lambda: cjson.loads(json_str))
+    print(f"{cjson_results['mean'] / json_results['mean']:.2f} of stdlib json time")
+    print(f"{cjson_results['mean'] / ujson_results['mean']:.2f} of ujson time")
+
+
+def testing_dumps(pydict: dict):
+    print("Testing 'dumps' method...\n")
+    print("\nTesting stdlib json 'dumps' performance:")
+    print("================================")
+    json_results = bench(lambda: json.dumps(pydict))
+
+    print("\nTesting ujson 'dumps' performance:")
+    print("==========================")
+    ujson_results = bench(lambda: ujson.dumps(pydict))
+    print(f"{ujson_results['mean'] / json_results['mean']:.2f} of stdlib json time")
+
+    print("\nTesting my very own cjson parser 'dumps' performance 😢:")
+    print("=========================================")
+    cjson_results = bench(lambda: cjson.dumps(pydict))
+    print(f"{cjson_results['mean'] / json_results['mean']:.2f} of stdlib json time")
+    print(f"{cjson_results['mean'] / ujson_results['mean']:.2f} of ujson time")
+
+
+def main():
+    with open("./data/sample.json") as fd:
+        json_str = fd.read()
+
+    testing_loads(json_str)
+    print("\n >>------------------------------------------------<<\n")
+    testing_dumps(json.loads(json_str))
+
+
+if __name__ == "__main__":
+    main()
